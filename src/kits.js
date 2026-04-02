772af61fca75c1b070d3ad7777e8c1546df2b944
Irene Lee <irenelee@ireneuicbookair.risd.edu>
2026-04-02 17:18:48 -0400

    fix all paths

--- src/kits.js
+++ src/kits.js
@@ -2,19 +2,22 @@ export const KITS = {
   v1: {
     id: 'v1',
     title: 'ENER - 1, BUILD ME',
-    model: '/models/build_me.glb',
-    background: 'dawn'
+    model: '/build_me/models/build_me.glb',
+    background: 'dawn',
+    image: '/build_me/images/v1_image.png',
   },
   v2: {
     id: 'v2',
     title: 'ENER 2, UPGRADE',
-    model: '/models/build_me2.glb',
-    background: 'blue_grass'
+    model: '/build_me/models/build_me2.glb',
+    background: 'blue_grass',
+    image: '/build_me/images/v2_image.png',
   },
   v3: {
     id: 'v3',
     title: 'ENER Z, eRr00orrrrorrz@#%7',
-    model: '/models/build_me3.glb',
-    background: 'glitch_blood'
+    model: '/build_me/models/build_me3.glb',
+    background: 'glitch_blood',
+    image: '/build_me/images/v3_image.png',
   }
 }
